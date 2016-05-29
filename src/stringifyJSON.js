c83a520fd842626d812700ccf18abaa796422805
donovanCarreno <donovan_carreno@yahoo.com>
2016-05-28 21:17:23 -0500

    implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null || 
+  	 typeof obj === 'number' || 
+  	 typeof obj === 'boolean') {
+  	return "" + obj + "";
+  }
+  else if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  }
+  else if (Array.isArray(obj)) {
+    var array = [];
+    for (var i = 0; i < obj.length; i++) {
+      array.push(stringifyJSON(obj[i]));
+    }
+    return '[' + array + ']';
+  }
+  else { 
+    var objVals = [];
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        objVals.push('"' + key + '":' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + objVals + '}';
+  }
 };
